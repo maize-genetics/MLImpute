a111e3991df4eeee1b7000bfbaa58beefef7f24c
Brandon <brandon.monier@gmail.com>
2025-08-01 18:08:50 -0500

    Add Vite + TS + React components

--- README.md
+++ README.md
@@ -1,36 +1,7 @@
-# MLImpute
-Simple tool to run Machine Learning based imputation techniques
+# Tauri + React + Typescript
 
+This template should help get you started developing with Tauri, React and Typescript in Vite.
 
-# Installation Conda Environment
+## Recommended IDE Setup
 
-```bash
-conda env create -f environment.yml
-```
-
-# Installation using pixi
-
-Install pixi if you haven't already:
-
-```bash
-curl -sSf https://pixi.sh/install.sh | bash
-```
-
-Then, you can install the MLImpute package using:
-
-```bash
-pixi install
-```
-
-
-# Run CLI Script
-
-Conda:
-```bash
-python impute.py --input <input_file> --output <output_file> --model <imputation_method>
-```
-
-Pixi:
-```bash
-pixi run -- python impute.py --input <input_file> --output <output_file> --model <imputation_method>
-```
+- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,30 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://v1.tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn greet_py(name: &str) -> String {
+    let output = std::process::Command::new("python3")
+        .arg("../src/greet.py")  // adjust path if needed
+        .arg(name)
+        .output()
+        .expect("Failed to execute Python script");
+
+    if output.status.success() {
+        String::from_utf8_lossy(&output.stdout).into_owned()
+    } else {
+        String::from_utf8_lossy(&output.stderr).into_owned()
+    }
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, greet_py])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
