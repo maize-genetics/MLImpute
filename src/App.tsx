80a181f68a0810aa16bf363ab93aecc51dae75cd
Brandon <brandon.monier@gmail.com>
2025-08-08 12:26:06 -0500

    Minor edits

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,27 @@ import { generateRandomMatrix, generateRandomHighlights } from "./components/uti
 
 
 function App() {
-  // dynamically generate a 100×50 matrix
   const { matrix: sampleMatrix, rowLabels: samples, colLabels: positions } =
-      generateRandomMatrix(50, 1000);
+    generateRandomMatrix(50, 1000);
 
   // Generate random highlights
   const highlights = generateRandomHighlights(samples, positions);
 
   return (
-      <main className="container">
-        <h1>ML Imputation App</h1>
-        <h2>(React + D3 Tests)</h2>
+    <main className="container">
+      <h1>ML Imputation App</h1>
+      <h2>(React + D3 Tests)</h2>
 
-        <D3Matrix
-            data={sampleMatrix}
-            rowLabels={samples}
-            colLabels={positions}
-            highlightData={highlights}
-            margin={{ top: 40, right: 10, bottom: 10, left: 80 }}
-            maxVisibleRows={20}
-            maxVisibleCols={50}
-        />
-      </main>
+      <D3Matrix
+        data={sampleMatrix}
+        rowLabels={samples}
+        colLabels={positions}
+        highlightData={highlights}
+        margin={{ top: 40, right: 10, bottom: 10, left: 80 }}
+        maxVisibleRows={20}
+        maxVisibleCols={50}
+      />
+    </main>
   );
 }
 
