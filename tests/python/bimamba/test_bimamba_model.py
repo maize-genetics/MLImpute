6101583d358819aebf36c067197b3849eab372de
smm477 <smm477@cornell.edu>
2025-08-13 12:26:44 -0400

    added test for forward

--- tests/python/bimamba/test_bimamba_model.py
+++ tests/python/bimamba/test_bimamba_model.py
@@ -1,6 +1,8 @@
 import unittest
 import torch
-from src.python.bimamba.bimamba_model import SNPLoss, SNPLossSmooth, SNPLossSmoothAll
+
+from src.python.bimamba.bimamba_model import SNPLoss, SNPLossSmooth, SNPLossSmoothAll, BiMambaSmooth
+
 
 class TestSNPLoss(unittest.TestCase):
     def setUp(self):
@@ -45,6 +47,24 @@ class TestSNPLossSmoothAll(unittest.TestCase):
 
     def test_not_nan(self):
         self.assertFalse(torch.isnan(self.loss))
+
+class TestModelForward(unittest.TestCase):
+    def setUp(self):
+        self.batch_size = 10
+        self.window_size = 512
+        self.num_classes = 25
+        self.input_dim = 25
+        self.d_model = 128
+        self.n_layer = 3
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = BiMambaSmooth(self.input_dim, self.d_model, self.num_classes, self.n_layer, d_conv=4, device=None, dtype=None, lambda_smooth=0.2)
+
+    def test_forward_shape(self):
+        self.model.to(self.device)
+        self.model.eval()
+        input = torch.randn(self.batch_size, self.window_size, self.num_classes, device=self.device)
+        outputs, mask = self.model(input)
+        self.assertEqual(outputs.shape, (torch.Size([self.batch_size, self.window_size, self.num_classes])))
 
 if __name__ == "__main__":
     unittest.main()
