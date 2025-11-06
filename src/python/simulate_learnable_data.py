bea6011d471041316fe737fce5feefff04aded2a
Ana Berthel <ahb232@cornell.edu>
2025-11-06 14:48:31 -0500

    add pre-trained model

--- src/python/simulate_learnable_data.py
+++ src/python/simulate_learnable_data.py
@@ -15,10 +15,11 @@ max_sd = 50
 min_dist = 100
 max_dist = 5000
 
-out_array = np.zeros((length, num_parents), dtype=int)
+out_array = np.zeros((length, num_parents+1), dtype=int)
 crossovers = []
 
 current_crossover = 0
+previous_hap = -1
 
 while current_crossover < length:
     next_crossover = current_crossover + np.random.randint(min_dist, max_dist)
@@ -28,6 +29,14 @@ while current_crossover < length:
         next_crossover = length
 
     crossovers.append(next_crossover)
+
+    haplotype = np.random.randint(0, num_parents)
+
+    while haplotype == previous_hap:
+        haplotype = np.random.randint(0, num_parents)
+
+    out_array[current_crossover:next_crossover, num_parents] = haplotype
+
     for idx in range(num_parents):
         mean = np.random.randint(min_mean, max_mean)
         sd = np.random.randint(min_sd, max_sd)
@@ -37,6 +46,7 @@ while current_crossover < length:
         out_array[current_crossover:next_crossover, idx] = dist
 
     current_crossover = next_crossover
+    previous_hap = haplotype
 
 np.save(out_file_name, out_array)
 np.save(label_file_name, np.array(crossovers, dtype=int))
--- src/python/train_viz.py
+++ src/python/train_viz.py
@@ -5,6 +5,9 @@ from transformers import Trainer, TrainingArguments
 from torch.utils.data import Dataset
 import numpy as np
 import torch.optim as optim
+from transformers.data.data_collator import InputDataClass
+from typing import Any
+from transformers.data.data_collator import default_data_collator
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -15,78 +18,132 @@ num_parents = 24
 chunks = image_size // num_parents
 pos_length = image_size * chunks
 
-config_decoder = ModernBertDecoderConfig(vocab_size=pos_length + 3, pad_token_id=pos_length, eos_token_id=pos_length + 1,
-                                  bos_token_id=pos_length + 2, cls_token_id=pos_length + 1, sep_token_id=pos_length + 2)
+pretrained_path=None
 
-encoder = ViTModel.from_pretrained("google/vit-base-patch16-384")
-decoder = ModernBertDecoderForCausalLM(config_decoder)
+if pretrained_path is not None:
+    model = VisionEncoderDecoderModel.from_pretrained(pretrained_path)
+else:
+    config_decoder = ModernBertDecoderConfig(vocab_size=pos_length + 3, pad_token_id=pos_length, eos_token_id=pos_length + 1,
+                                      bos_token_id=pos_length + 2, cls_token_id=pos_length + 1, sep_token_id=pos_length + 2)
 
-model = VisionEncoderDecoderModel(encoder=encoder, decoder=decoder)
-model.config.decoder_start_token_id = pos_length+2
-model.config.pad_token_id = pos_length
-model.config.vocab_size = pos_length+3
-model.config.eos_token_id = pos_length+1
+    encoder = ViTModel.from_pretrained("google/vit-base-patch16-384")
+    decoder = ModernBertDecoderForCausalLM(config_decoder)
 
+    model = VisionEncoderDecoderModel(encoder=encoder, decoder=decoder)
+    model.config.decoder_start_token_id = pos_length+2
+    model.config.pad_token_id = pos_length
+    model.config.vocab_size = pos_length+3
+    model.config.eos_token_id = pos_length+1
+
+
+def custom_data_collator(features: list[InputDataClass]) -> dict[str, Any]:
+    batch = default_data_collator(features)
+    batch["return_dict"] = False
+
+    return batch
 
 class SegmentationDataset(Dataset):
-    def __init__(self, input_file, label_file, image_size=384, num_parents=24, step_size=1536):
+    def __init__(self, input_file_names, image_size=384, num_parents=24, step_size=1536):
+        self.input_file_names = input_file_names
         self.window_size = (image_size * image_size) // num_parents
         self.image_size = image_size
         self.num_parents = num_parents
         self.step_size = step_size
-        self.matrix = np.load(input_file)[:, 0:num_parents]
-        # self.labels = np.load(label_file)
+        self.windows = self.__generate_windows__()
 
-        self.n_windows = (self.matrix.shape[0] - self.window_size) // step_size
+        self.n_windows = len(self.windows)
+
+    def __generate_windows__(self):
+        windows = [] # file idx, window step idx
+
+        for idx in range(len(self.input_file_names)):
+            filelen = np.load(self.input_file_names[idx]).shape[0]
+
+            num_windows = (filelen - self.window_size) // self.step_size
+
+            windows.extend([(idx, idy) for idy in range(num_windows)])
+
+        return windows
 
     def __len__(self):
         return self.n_windows
 
+    def __bins_to_idx__(self, labels_binned):
+        return [idx+1 for idx in range(labels_binned.shape[0] - 1) if labels_binned[idx] != labels_binned[idx+1]]
+
     def __getitem__(self, idx):
-        pos_start = idx * self.step_size
+        file_idx, pos_idx = self.windows[idx]
+
+        pos_start = pos_idx * self.step_size
         pos_end = pos_start + self.window_size
 
-        window = np.hstack(np.split(self.matrix[pos_start:pos_end], self.window_size // self.image_size))
+        ip = np.load(self.input_file_names[file_idx], allow_pickle=True, mmap_mode='r')[pos_start:pos_end]
 
-        # TODO: normalize
-        # ViT wants rgb channels each with mean 0.5 and sd 0.5
+        matrix = ip[:, 0:num_parents]
 
-        # TODO: import real junction data
+        # normalize the matrix according to ViT's requirements (mean 0.5 std 0.5)
+        mean = np.mean(matrix)
+        sd = np.std(matrix)
 
-        junctions = np.random.randint(0, self.window_size, np.random.randint(0, 10))
+        matrix = (matrix - mean) / sd
 
-        if junctions.shape[0] < 14:
+        matrix = matrix * 0.5 + 0.5
+
+        labels_binned = ip[:, num_parents]
+
+        junctions = self.__bins_to_idx__(labels_binned)
+
+        window = np.hstack(np.split(matrix, self.window_size // self.image_size))
+
+        # for now, create greyscale image by stacking the matrix 3 times
+        # later we'll try encoding different information in each layer
+
+        window = np.stack((window, window, window), axis=0)
+
+        if len(junctions) < 62:
+            mask = np.concatenate((np.zeros(62 - len(junctions)), np.ones(len(junctions) + 2)))
             junctions = np.concatenate(
-                ([self.window_size + 2], junctions, [self.window_size + 1], [3456] * (14 - junctions.shape[0])))
+                ([self.window_size] * (62 - len(junctions)), [self.window_size + 2], junctions, [self.window_size + 1]))
         else:
+            mask = np.ones(64)
             junctions = np.concatenate(([self.window_size + 2], junctions, [self.window_size + 1]))
 
         return {
-            'pixel_values': torch.tensor(window.unsqueeze(0), dtype=torch.float),
-            'labels': torch.tensor(junctions).squeeze()
+            'pixel_values': torch.tensor(window, dtype=torch.float),
+            'labels': torch.tensor(junctions, dtype=torch.int64).squeeze(),
+            'decoder_attention_mask': mask
         }
 
 
-dataset = SegmentationDataset("/workdir/ahb232/MLImpute/src/training_data/train/CML442_matrix.npy", "bar.txt")
+dataset = SegmentationDataset(["/workdir/ahb232/MLImpute/data/mock_data/sample_A.npy",
+                               "/workdir/ahb232/MLImpute/data/mock_data/sample_B.npy",
+                               "/workdir/ahb232/MLImpute/data/mock_data/sample_C.npy"])
+
 dataset_chunks = torch.utils.data.random_split(dataset, [0.9, 0.1])
 dataset_train = dataset_chunks[0]
 dataset_val = dataset_chunks[1]
 
 optimizer = optim.AdamW(model.parameters())
-lr_scheduler = get_wsd_schedule(optimizer, 100, 100, num_training_steps=len(dataset_train) // 16)
 
 model = model.to(device)
 model.train()
+
+num_epochs = 2
+
+len_warmup = (len(dataset_train) * num_epochs) // 320
+
+lr_scheduler = get_wsd_schedule(optimizer, len_warmup, len_warmup, num_training_steps=(num_epochs * len(dataset_train)) // 32)
+
 training_args = TrainingArguments(
-    output_dir='./results',
-    num_train_epochs=10,
+    output_dir='./results_epoch_0_1',
+    num_train_epochs=num_epochs,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=64,
-    #    warmup_steps=500,
     weight_decay=0.01,
     logging_dir='./logs',
     logging_steps=10,
-    report_to="wandb"
+    report_to="wandb",
+    run_name="test_epochs_0-1"
 )
 
 trainer = Trainer(
@@ -94,6 +151,7 @@ trainer = Trainer(
     args=training_args,
     train_dataset=dataset_train,
     eval_dataset=dataset_val,
+    data_collator=custom_data_collator,
     optimizers=(optimizer, lr_scheduler)
 )
 trainer.train()
