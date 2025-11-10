bcc723dd75764c0c7c60cd934591ff7a66f4bc14
smm477 <smm477@cornell.edu>
2025-11-10 09:44:50 -0500

    added HMM arg

--- src/python/impute.py
+++ src/python/impute.py
@@ -54,7 +54,7 @@ def main():
 
     parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose logging")
     parser.add_argument("--global-weights", type=str, default=None)
-    parser.add_argument("--HMM", type=bool, default=False)
+    parser.add_argument("--hmm", type=bool, default=False)
     parser.add_argument("--diploid", type=bool, default=False)
     parser.add_argument("--window-size", type=int, default=21, help="Size of the sliding window for KNN model (must be odd)")
 
