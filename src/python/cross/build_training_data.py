0996e03309d838e6063772cd3151ced7d626bd25
smm477 <smm477@cornell.edu>
2025-12-09 16:35:36 -0500

    fixed import

--- src/python/cross/build_training_data.py
+++ src/python/cross/build_training_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from src.python.ps4g_io.ps4g import load_ps4g_file, extract_metadata
-from src.python.cross.chrom_lengths import chrom_lengths
+from python.ps4g_io.ps4g import load_ps4g_file, extract_metadata
 from tqdm import tqdm
 import argparse
 import os
